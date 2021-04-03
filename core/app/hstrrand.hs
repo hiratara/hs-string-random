2eaf4bcd1e6a1658947067a5b2f2f9f9ed4d2960
Masahiro Honma <hiratara@cpan.org>
2021-04-03 11:53:22 +0900

    chore: auto formatting

--- core/app/hstrrand.hs
+++ core/app/hstrrand.hs
@@ -1,17 +1,20 @@
 {-# LANGUAGE Strict #-}
+
 module Main (main) where
 
 import Data.Monoid ((<>))
-import qualified Options.Applicative as Options
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
+import qualified Options.Applicative as Options
 import qualified Text.StringRandom as StringRandom
 
 argParser :: Options.Parser Text.Text
-argParser = Text.pack <$> Options.strArgument
-         ( Options.metavar "REGEXP"
-        <> Options.help "Regexp as a template (e.g. '[1-3]{2}random[!?]')"
-         )
+argParser =
+  Text.pack
+    <$> Options.strArgument
+      ( Options.metavar "REGEXP"
+          <> Options.help "Regexp as a template (e.g. '[1-3]{2}random[!?]')"
+      )
 
 main :: IO ()
 main = do
@@ -19,6 +22,9 @@ main = do
   txt <- StringRandom.stringRandomIO pat
   Text.putStrLn txt
   where
-    opts = Options.info (Options.helper <*> argParser)
-      ( Options.fullDesc
-     <> Options.header "hstrrand - Generate string which matches REGEXP" )
+    opts =
+      Options.info
+        (Options.helper <*> argParser)
+        ( Options.fullDesc
+            <> Options.header "hstrrand - Generate string which matches REGEXP"
+        )
